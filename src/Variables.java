b30119d629cd99a7ffcedd14a1e5caffa888085e
DJBostock <douglasjbostock@gmail.com>
2022-07-16 20:22:51 -0500

    more lessons about variables

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Variables {
+public class Variables {
+    public static void main(String[] args) {
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println("My first number is " + myFirstNumber + ".");
+
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("My total number is " + myTotal + ".");
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println("The value of myLastOne is: " + myLastOne + ".");
+    }
 }
