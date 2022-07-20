9d0846fac84228d98644898b88e5395a19a4450b
DJBostock <douglasjbostock@gmail.com>
2022-07-20 18:16:57 -0500

    floating point precision lesson

--- src/FloatAndDouble.java
+++ src/FloatAndDouble.java
@@ -11,8 +11,15 @@ public class FloatAndDouble {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+        double numPounds = 25d;
+        double numKilograms = numPounds * 0.45359237d;
+        System.out.println(numPounds + " pounds equals " + numKilograms + " kilograms.");
     }
 }
